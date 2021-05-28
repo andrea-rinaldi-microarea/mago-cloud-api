3ee9bef84627c620d5d367ef4fbb11d9fee8b29f
andrea-rinaldi-microarea <andrea.rinaldi@microarea.it>
2021-05-28 17:52:14 +0200

    first attempt, not completed actual nuget package on .NET Standard 2.1 do not support apps in .NET Framework

---
+++ mago-api-client/MagoAPIClientSample/MainForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace MagoAPIClientSample
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.tbxAccountName = new System.Windows.Forms.TextBox();
+            this.tbxSubscription = new System.Windows.Forms.TextBox();
+            this.tbxRootURL = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.Location = new System.Drawing.Point(106, 91);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.PasswordChar = '*';
+            this.tbxPassword.Size = new System.Drawing.Size(141, 20);
+            this.tbxPassword.TabIndex = 29;
+            // 
+            // tbxAccountName
+            // 
+            this.tbxAccountName.Location = new System.Drawing.Point(106, 65);
+            this.tbxAccountName.Name = "tbxAccountName";
+            this.tbxAccountName.Size = new System.Drawing.Size(141, 20);
+            this.tbxAccountName.TabIndex = 28;
+            // 
+            // tbxSubscription
+            // 
+            this.tbxSubscription.Location = new System.Drawing.Point(106, 39);
+            this.tbxSubscription.Name = "tbxSubscription";
+            this.tbxSubscription.Size = new System.Drawing.Size(141, 20);
+            this.tbxSubscription.TabIndex = 27;
+            // 
+            // tbxRootURL
+            // 
+            this.tbxRootURL.Location = new System.Drawing.Point(106, 12);
+            this.tbxRootURL.Name = "tbxRootURL";
+            this.tbxRootURL.Size = new System.Drawing.Size(202, 20);
+            this.tbxRootURL.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "account name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "subscription";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "MagoCloud URL";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(106, 133);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(96, 23);
+            this.btnConnect.TabIndex = 30;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tbxPassword);
+            this.Controls.Add(this.tbxAccountName);
+            this.Controls.Add(this.tbxSubscription);
+            this.Controls.Add(this.tbxRootURL);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "MagoAPIClient Sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.TextBox tbxAccountName;
+        private System.Windows.Forms.TextBox tbxSubscription;
+        private System.Windows.Forms.TextBox tbxRootURL;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ mago-api-client/MagoAPIClientSample/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MagoAPIClientSample
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            //    ITbUserData userData = null;
+            //    try
+            //    {
+            //        using (MagoAPIClient magocloudClient = new MagoAPIClient(instance, new ProducerInfo("producer key", "app ID")))
+            //        {
+            //        }
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine(ex);
+            //        if (userData != null && magocloudClient.AccountManager.IsValid(userData).Result.Success)
+            //            return magocloudClient.AccountManager.Logout(userData).Result.Success;
+            //    }
+        }
+    }
+}
